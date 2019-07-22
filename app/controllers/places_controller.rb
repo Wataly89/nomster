cbeb063eaca5a91180f22a50f13e1995f57adc89
Alyse <awatton89@gmail.com>
2019-07-22 23:59:08 +0000

    vailidation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,31 @@
 class PlacesController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
+
 	def index
 		@places = Place.order("name").page(params[:page]).per_page(3)
 	end
+
 
 	def new
 		@place = Place.new
 	end
 
+
 	def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
 
   def show
   	@place = Place.find(params[:id])
   end
+
 
   def edit
   	@place = Place.find(params[:id])  	
@@ -25,28 +34,35 @@ class PlacesController < ApplicationController
 		 end
   end
 
+
   def update
   	@place = Place.find(params[:id])
   	if @place.user != current_user
   	    return render plain: 'Not Allowed', status: :forbidden
   	 end
 
-
   	@place.update_attributes(place_params)
-  	redirect_to root_path
+  	if @place.valid?
+  	   redirect_to root_path
+  	 else
+  	   render :edit, status: :unprocessable_entity
+  	 end
   end
+
 
   def destroy
     @place = Place.find(params[:id])
     if @place.user != current_user
     	return render plain: 'Not Allowed', status: :forbidden
     end
-    
+
     @place.destroy
     redirect_to root_path
   end
 
+
   private
+
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
