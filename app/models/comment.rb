5bc9ead3582cb543e3d68bb17a843ab9113bd9e2
Alyse <awatton89@gmail.com>
2019-08-06 23:08:53 +0000

    test emails

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,17 +1,25 @@
 class Comment < ApplicationRecord
 	belongs_to :user
 	belongs_to :place
+	after_create :send_comment_email
 
 
 	RATINGS = {
-	    'One star': '1_star',
-	    'Two stars': '2_stars',
-	    'Three stars': '3_stars',
-	    'Four stars': '4_stars',
-	    'Five stars': '5_stars'
-	  }
+		'One star': '1_star',
+		'Two stars': '2_stars',
+		'Three stars': '3_stars',
+		'Four stars': '4_stars',
+		'Five stars': '5_stars'
+	}
 
-	  def humanized_rating
-	      RATINGS.invert[self.rating]
-	    end
+	def humanized_rating
+		RATINGS.invert[self.rating]
+	end
+
+	def send_comment_email
+		NotificationMailer.comment_added(self).deliver_now
+		
+	end
+
+
 end
